394e23ffd27bda1a3ef9b09e0e8d453bbe984cc0
Fleksis <markuss0303@gmail.com>
2022-12-15 01:58:54 +0200

    feat: temp3

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-    <h2>dasdklsjdkas</h2>
+    <h2>{{ this.$auth.$state.user }}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ProfilePage',
-  layout: 'UserNavigationBar',
-  auth: true,
-  data () {
-    return {
-      user: this.$auth.$state.user,
-    }
-  }
+  auth: true
 }
 </script>
 
