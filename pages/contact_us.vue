4eb66d3eedd1451627475ded2bebb2884bcce796
tekuto1437 <denisjerosenko79@gmail.com>
2022-12-30 14:36:18 +0200

    feat:test

---
+++ pages/contact_us.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>asdasdazxc asdas </p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: "ContactUs"
+}
+</script>
+
+<style scoped>
+p{
+    color: red;
+}
+
+</style>
