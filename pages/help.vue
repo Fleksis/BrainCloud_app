145a2ceee8c3df692aa4ef371210c840a833d1eb
Krasnikovs <ralis3333@gmail.com>
2023-03-07 20:26:50 +0200

    feat:/Help page redesign

--- pages/help.vue
+++ pages/help.vue
@@ -1,225 +1,25 @@
 <template>
   <div class="container">
-    <div class="help-title"><h1>Help page</h1></div>
-    <div class="subscription">
-      <div><h2 style="color: white">Deniss</h2></div>
-      <h2>Folder</h2>
-      <p>Folders are usually needed to group files, but for us folders are needed to start uploading files to ‘Brain-cloud’. They are a renamable way to organize files with an option to make up to 20 folders for the Free plan.</p>
-      <h3>Create</h3>
-      <ol type="1">
-        <li>To create a folder, you need to press the ‘Create Folder’ button.</li>
-        <li>A small window will appear under the ‘Create Folder’ button, where it will ask to input the folder name. To confirm the name press ‘Create’.</li>
-      </ol>
-      <h3>Delete</h3>
-      <ol type="1">
-        <li>There will be a button that looks like a folder that flashes with a red X when you hover over it, that appears next to the folder name.</li>
-        <li>When you press the button, a confirmation popup appears and to procced you need to press the ‘Delete’ button.</li>
-      </ol>
-      <h3>Rename</h3>
-      <ol type="1">
-        <li>To rename you will need to press the button that looks like a pencil.</li>
-        <li>When you press the button, a confirmation popup appears where you can enter a new name for the folder and to procced you need to press the ‘Change’ button.</li>
-      </ol>
-    </div>
-    <div class="subscription">
-      <div><h2 style="color: white">Aleksandrs</h2></div>
-      <h2>
-        File Upload
-      </h2>
-      <p>You can choose a folder where you want to upload the file. There is an option to upload a file through file explorer or by dragging in the file. Search for your file, by opening the folder where your file is saved, and typing the name or description of the file. Clicking on a file you can see its data like its title, description, upload data, size. Also clicking on a file you can download, rename or delete it.</p>
-      <h3>File Upload</h3>
-      <ol type="1">
-        <li>Open the folder where you want to upload the file (You must have at least one folder).</li>
-        <li>Press the button ‘Add File’.</li>
-        <li>In opened window, select your file which you want to upload and press ‘Ok’.</li>
-        <li>In the ‘Title’ field write name for your file and in the ‘Description’ field write about your file.</li>
-        <li>To finish your file upload, press the button ‘Create’.</li>
-      </ol>
-      <h3>File Search and View</h3>
-      <ol type="1">
-        <li>Click on the search bar.</li>
-        <li>Type the file name or description.</li>
-        <li>When your file will be found, click on him.</li>
-        <li>Then you can see its data.</li>
-      </ol>
-      <h3>File Dowload</h3>
-      <ol type="1">
-        <li>Click on your desired file.</li>
-        <li>In the opened window, you can see ‘Dowload’ button, click on it.</li>
-        <li>In the opened panel, select ‘Save As..’ Function.</li>
-        <li>Select folder on your desktop where you want to save file.</li>
-        <li>Press button ‘Save’ and finish file dowload.</li>
-      </ol>
-      <h3>File Rename</h3>
-      <ol type="1">
-        <li>Click on your file.</li>
-        <li>In the opened window, you can see ‘Rename’ button , click on it.</li>
-        <li>In next opened window, you can see ‘Title’ and ‘Description’ fields.</li>
-        <li>In ‘Title’ field write the files new name.</li>
-        <li>In the ‘Description’ field write a new description for your file.</li>
-        <li>To save the changes, press the button ‘Change’.</li>
-        <li>Changes will be accepted.</li>
-      </ol>
-      <h3>File Delete</h3>
-      <ol type="1">
-        <li>Click on your file.</li>
-        <li>In the opened window, you can see the ‘Delete’ button, click on it.</li>
-        <li>In the opened window press ‘Delete’.</li>
-        <li>The file will be deleted.</li>
-      </ol>
-    </div>
-    <div class="subscription">
-      <div><h2 style="color: white">Gustavs</h2></div>
-      <h2>
-        Personal data update
-      </h2>
-      <p>With account settings you can refresh your picture, username, email and password.</p>
-      <h3>Update picture</h3>
-      <ol type="1">
-        <li>Click on your profile icon, on top-right corner of the page.</li>
-        <li>Click the ‘Account settings’ button.</li>
-        <li>To change your profile picture, click ‘Personal Data’.</li>
-        <li>Then press the ‘Change avatar’.</li>
-        <li>File explore will pop up, so you can choose what will be your new image.</li>
-        <li>To confirm your actions, press ‘Save’.</li>
-      </ol>
-      <h3>Update Username/Email/Password</h3>
-      <ol type="1">
-        <li>Click on your profile icon, on top-right corner of the page.</li>
-        <li>Click the ‘Account settings’ button.</li>
-        <li>To change your Username/Email/Password, click ‘Personal Data’.</li>
-        <li>And type in your new data in the corresponding inputs.</li>
-        <li>To confirm your actions, press ‘Save’.</li>
-      </ol>
-      <h2>About us</h2>
-      <p>In about us you can try contacting us and see what we consider our core values.</p>
-      <h3>Contact Us</h3>
-      <ol type="1">
-        <li>To reach ‘About us’ page, press ‘About us’ button on the navigation bar.</li>
-        <li>Scroll down to the bottom, so you can see Contact us fully.</li>
-        <li>Enter your First name, Last name, Email, then enter the topic you want to know about and them you can enter your description.</li>
-        <li>To send it, press ‘Submit’ button.</li>
-      </ol>
-      <h2>Admin page</h2>
-      <p>Ability to see all the users, change their data, upgrade their subscription and delete them.</p>
-      <h3>Rename</h3>
-      <ol type="1">
-        <li>Go to ‘Admin page’ page, by pressing ‘Admin page’ on the navigation bar.</li>
-        <li>Press ‘More’ on the desirable user.</li>
-        <li>Click on ‘Rename’ and change their data.</li>
-        <li>To confirm press ‘Create’.</li>
-      </ol>
-      <h3>Delete</h3>
-      <ol type="1">
-        <li>Go to ‘Admin page’ page, by pressing ‘Admin page’ on the navigation bar.</li>
-        <li>Press ‘More’ on the desirable user.</li>
-        <li>Click ‘Delete’ and then click on ‘Delete’.</li>
-      </ol>
-      <h3>Reset password</h3>
-      <ol type="1">
-        <li>Go to ‘Admin page’ page, by pressing ‘Admin page’ on the navigation bar.</li>
-        <li>Press ‘More’ on the desirable user.</li>
-        <li>Click ‘Reset password’ and then click on ‘Reset’.</li>
-      </ol>
-      <h2>
-          Subscription
-      </h2>
-      <p>
-          For subscription there are 4 types of plans Free/Basic/Extra/Rugged storage.
-          Each of them can provide benefits like more storage,
-          options to create more folders and files while uploding bigger files.
-          You are automatically subscribed to the Free option. To upgrade your abilities,
-          you can choose to buy a plan for a respective price of 9.99/14.99/19.99 €.
-      </p>
-      <h3>
-          Upgrade
-      </h3>
-      <ol type="1">
-          <li>
-              Go to the ‘Subscription’ page.
-          </li>
-          <li>
-              Choose an appropriate option for your needs. For example, like the ‘Basic’ plan.
-          </li>
-          <li>
-              Next your, going to be redirected to a payment page.
-          </li>
-          <li>
-              Fill out the available inputs and press ‘Pay’ to proceed.
-          </li>
-          <li>
-              Then you will be readdressed to the ‘Home’ page and a popup should appear with a message ‘You successfully upgraded your plan’.
-          </li>
-      </ol>
-      <ul>
-          <li>
-              You can recheck if you have successfully enhanced your plan, by pressing on your user icon. If successful under the storage bar should be the new storage limit.
-          </li>
-          <li>
-              It is possible to get a popup with a message ‘Payment failed’. By clicking on the back button in the payment page.
-          </li>
-      </ul>
-    </div>
+    <help-instruction-box
+      title="How to asd1"
+      description="asd2 is something that you need to use very carefully "
+      :steps="['asd1','asd2','asd3','asd4']"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: 'help',
-  layout: 'UserNavigationBar',
+  layout: 'NotUserNavigationBar',
   auth: false
 }
 </script>
 
 <style scoped>
 .container {
-  margin-top: 60px;
+  margin-top: 100px;
   width: 70%;
   margin-inline: auto;
-}
-
-.help-title {
-  width: 100%;
-}
-
-.help-title > h1 {
-    color: #6C63FF;
-    font-weight: normal;
-    text-align: center;
-    font-family: Alata;
-}
-
-.subscription > h2 {
-    color: #6C63FF;
-    font-weight: normal;
-    font-family: Alata;
-}
-
-.subscription > p {
-    width: 90%;
-    margin-inline: auto;
-    color: white;
-    font-family: Alata;
-}
-
-.subscription > h3 {
-    color: #6C63FF;
-    margin-left: 50px;
-    font-weight: normal;
-    font-family: Alata;
-}
-
-.subscription > ol {
-    width: 86%;
-    margin-inline: auto;
-    color: white;
-    font-family: Alata;
-}
-
-.subscription > ul {
-    width: 79%;
-    margin-inline: auto;
-    color: white;
-    font-family: Alata;
 }
 </style>
